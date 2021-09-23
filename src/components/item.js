e84c6c277ed3207e69cdc8736ad3811d84822623
fangbo <649392395@qq.com>
2021-09-23 17:43:51 +0800

    cc

---
+++ src/components/item.js
@@ -0,0 +1,19 @@
+var c = []
+let a = ['10233']
+let b = [{
+        palce: '武汉市',
+        placeId: '10233'
+    },
+    {
+        palce: '北京市',
+        placeId: '10433'
+    }, {
+        palce: '宁波市',
+        placeId: '102233'
+    }
+]
+function name() {
+    b.map(item => {item.placeId})
+    
+}
+name()
